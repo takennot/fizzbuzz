ba04bb84236f2d827873322616245f9069e313ef
Ruslan Musaev <rus.musaev1996@gmail.com>
2024-06-24 22:43:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace FizzBuzz;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input != null && Int32.TryParse(input, out int value))
+            {
+                int n = Int32.Parse(input);
+                FizzBuzz(n);
+            }
+            else if (input.Equals("exit"))
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+
+    public static void FizzBuzz(int n)
+    {
+        Console.WriteLine("___________________________");
+        for (int i = 0; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.WriteLine("___________________________");
+    }
+}
